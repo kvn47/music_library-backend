c26a13b49e2bf496604fde52e03bbded4ea3e3ac
Vladimir Kochergin <kvn047@gmail.com>
2018-07-03 10:56:39 +1000

    Styling

--- app/concepts/track/representer/base.rb
+++ app/concepts/track/representer/base.rb
@@ -9,9 +9,14 @@ module Track::Representer
     property :title
     property :number
     property :size, exec_context: :decorator
+    property :artist_name, exec_context: :decorator
 
     def size
       number_to_human_size represented.size
+    end
+
+    def artist_name
+      represented.artist.name
     end
   end
 end
